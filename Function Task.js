3d01ac4922637b52530fa2e53b8d91b74d0bd417
Kofoworolastephanie <101248957+Kofoworolastephanie@users.noreply.github.com>
2022-03-09 22:16:48 +0100

    precious

---
+++ Function Task.js
@@ -0,0 +1,6 @@
+function compareNames(nameOne, nameTwo) {  
+   return nameOne. toLowerCase() === nameTwo. toLowerCase() }
+
+
+   compareNames("Ayodele Kofoworola", "Ayodele Stephanie Kofoworola")
+   compareNames("Ayodele Kofoworola", "Ayodele Kofoworola")
